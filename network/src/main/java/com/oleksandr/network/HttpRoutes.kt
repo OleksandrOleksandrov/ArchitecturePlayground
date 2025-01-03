617cf9179bfd3bf9f4c9c9a6b9e928984bef8afe
Aleksandrov Aleksandr <AleksandrAleksandrovUA@gmail.com>
2025-01-03 14:36:18 +0200

    Implement network module

--- network/src/main/java/com/oleksandr/network/HttpRoutes.kt
+++ network/src/main/java/com/oleksandr/network/HttpRoutes.kt
@@ -2,10 +2,10 @@ package com.oleksandr.network
 
 object HttpRoutes {
 
-    const val NASA_BASE_URL = BuildConfig.NASA_BASE_URL
+    private const val NASA_BASE_URL = BuildConfig.NASA_BASE_URL
 
     // Auth region
-    const val TEST = "$NASA_BASE_URL/DONKI/CME?startDate=2017-01-03&endDate=2017-01-03&api_key=DEMO_KEY"
+    const val EPIC_PICTURES = "$NASA_BASE_URL/EPIC/api/natural?api_key=DEMO_KEY"
     // Auth endregion
 
     private fun String.urlPath() = this.removePrefix(NASA_BASE_URL)
