92ef849406ae5db7135f3ca28c052ce1bfe07d3e
Oleksandrov Oleksandr <oleksandr.oleksandrov@devlight.io>
2025-06-27 17:29:09 +0300

    update libs

--- feature-earth-polychromatic-imaging-camera/src/main/java/com/oleksandr/epic/di/FeatureEarthPolychromaticImagingCamera.kt
+++ feature-earth-polychromatic-imaging-camera/src/main/java/com/oleksandr/epic/di/FeatureEarthPolychromaticImagingCamera.kt
@@ -1,7 +1,7 @@
 package com.oleksandr.epic.di
 
 import com.oleksandr.epic.screen.EPICViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val featureEarthPolychromaticImagingCameraModule = module {
--- feature-epic-details/src/main/java/com/oleksandr/epic/details/di/FeatureEpicDetails.kt
+++ feature-epic-details/src/main/java/com/oleksandr/epic/details/di/FeatureEpicDetails.kt
@@ -2,8 +2,8 @@ package com.oleksandr.epic.details.di
 
 import com.oleksandr.epic.details.mapper.EPICDomainUiModelMapper
 import com.oleksandr.epic.details.screen.EpicDetailsViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val featureEpicDetailsModule = module {
