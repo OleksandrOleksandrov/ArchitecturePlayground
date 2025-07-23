2ac612822ea0b7d0b3b954f83fad99d4c5a13775
Oleksandrov Oleksandr <oleksandr.oleksandrov@devlight.io>
2025-07-23 09:40:35 +0300

    Small UI tweaks

--- feature-epic-details/src/main/java/com/oleksandr/epic/details/screen/EpicDetailsContent.kt
+++ feature-epic-details/src/main/java/com/oleksandr/epic/details/screen/EpicDetailsContent.kt
@@ -1,6 +1,5 @@
 package com.oleksandr.epic.details.screen
 
-import android.net.Uri
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.core.net.toUri
 import coil.compose.AsyncImage
 import com.oleksandr.presentation.core.model.EpicUiModel
 
@@ -25,7 +25,7 @@ fun EpicDetailsContent(
                 item.caption.orEmpty() + item.date,
             )
             AsyncImage(
-                model = Uri.parse(item.image),
+                model = item.image?.toUri(),
                 contentScale = ContentScale.FillWidth,
                 contentDescription = null,
                 alignment = Alignment.CenterStart,
--- feature-epic-details/src/main/java/com/oleksandr/epic/details/screen/EpicDetailsScreen.kt
+++ feature-epic-details/src/main/java/com/oleksandr/epic/details/screen/EpicDetailsScreen.kt
@@ -1,22 +1,34 @@
 package com.oleksandr.epic.details.screen
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.oleksandr.epic.details.navigation.EpicDetailsDirections
 import com.oleksandr.presentation.core.model.EpicUiModel
+import com.oleksandr.presentation.styling.theme.core.Theme
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun EpicDetailsScreen(model: EpicDetailsDirections.EpicDetails) {
 
     val viewModel: EpicDetailsViewModel = koinViewModel()
-    EpicDetailsContent(
+
+    Scaffold(
         modifier = Modifier,
-        EpicUiModel(
-            identifier = model.identifier,
-            caption = model.caption,
-            image = model.image,
-            date = model.date,
-        ),
-    )
+        contentWindowInsets = WindowInsets.safeDrawing,
+        containerColor = Theme.color.themeNColor.n50,
+    ) { scaffoldPaddingValues ->
+        EpicDetailsContent(
+            modifier = Modifier.padding(scaffoldPaddingValues),
+            EpicUiModel(
+                identifier = model.identifier,
+                caption = model.caption,
+                image = model.image,
+                date = model.date,
+            ),
+        )
+    }
 }
