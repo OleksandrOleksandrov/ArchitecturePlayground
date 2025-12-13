a8341e0e63351f8a94ad82de8391d01c358df74d
Oleksandrov Oleksandr <aleksandraleksandrovua@gmail.com>
2025-12-13 22:53:33 +0200

    Revert "Put demo api key into secrets"
    This reverts commit 81d28631855de1dfd1593d2c7432bee2b4c7020e.

--- network/src/main/java/com/oleksandr/network/HttpRoutes.kt
+++ network/src/main/java/com/oleksandr/network/HttpRoutes.kt
@@ -4,14 +4,12 @@ object HttpRoutes {
 
     private const val NASA_BASE_URL = BuildConfig.NASA_BASE_URL
 
-    private const val API_KEY = BuildConfig.NASA_API_KEY
-
     // Auth region
     // TODO replace with dynamic api_key
-    const val EPIC_PICTURE_LIST = "https://epic.gsfc.nasa.gov/api/natural?api_key=$API_KEY"
+    const val EPIC_PICTURE_LIST = "https://epic.gsfc.nasa.gov/api/natural?api_key=3SXKZBBq6vEmA7yamongiBY66cvnlx1JbeeDOtZu"
     // APOD - Astronomy Picture of the Day
     // TODO replace with dynamic api_key
-    const val APOD = "$NASA_BASE_URL/planetary/apod?api_key=$API_KEY"
+    const val APOD = "$NASA_BASE_URL/planetary/apod?api_key=3SXKZBBq6vEmA7yamongiBY66cvnlx1JbeeDOtZu"
     // Auth endregion
 
     private fun String.urlPath() = this.removePrefix(NASA_BASE_URL)
